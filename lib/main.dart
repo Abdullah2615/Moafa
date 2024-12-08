a7a3e52f747fd9f17fea295cecdaa35b13641d17
Mahmoud Mostafa <mahmoudwahdan121@gmail.com>
2024-12-08 19:45:22 +0200

    Gradle Fix 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:doctor/veiw/addpills.dart';
+import 'package:doctor/veiw/welcom.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
@@ -19,6 +20,6 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: Pill());
+        home: Start());
   }
 }
